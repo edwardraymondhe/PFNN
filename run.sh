ba186095d1e9f304eee307a9acdd7f24d5bf252c
edwardraymondhe <43778680+edwardraymondhe@users.noreply.github.com>
2025-02-18 10:35:45 +0800

    feat: patch command for ease

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# conda init
+# conda activate games105
+
+cd ./demo
+make
+cd ../
+
+rm -rf ./demo/network/pfnn
+mkdir ./demo/network/pfnn
+python preprocess_footstep_phase.py
+python generate_patches.py
+python generate_database.py
+python train_pfnn.py
